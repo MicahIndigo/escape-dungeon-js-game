cd209a5841230243b51429e80a28f9805ffc3d17
MicahIndigo <micah.music96@gmail.com>
2025-08-05 19:44:42 +0100

    Add function to shuffle cards

--- assets/js/games/memory.js
+++ assets/js/games/memory.js
@@ -33,5 +33,15 @@ function resetMemoryState() {
     matchedPairs = 0;
     attemptsLeft = 3;
     timeRemaining = 45;
+    clearInterval(memoryTimer);
     puzzleGameContainer.innerHTML = '';
+}
+
+/* Shuffle the cards(symbols) using Fisher-Yates algorithm */
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
 }

6db2122dfb501c772c99bf0d1e79ca6db0167e9e
MicahIndigo <micah.music96@gmail.com>
2025-08-05 19:17:27 +0100

    Add function to start memory puzzle

--- assets/js/games/memory.js
+++ assets/js/games/memory.js
@@ -5,3 +5,23 @@ const memoryImages = [
     '🗝️', '❤️', '🔒', '🧩',
     '🗝️', '❤️', '🔒', '🧩' // Each symbols appear twice
 ];
+let shuffledCards = [];
+let flippedCards = [];
+let matchedPairs = 0;
+let attemptsLeft = 3;
+let memoryTimer = null;
+let timeRemaining = 30;
+
+/* Dom References */
+const puzzleGameContainer = document.getElementById("puzzleGameContainer");
+
+/* Function to start the Memory game called in story.js */
+function startMemory(onSuccess, onFail) {
+    // Store the callbacks so puzzleResult can use them later
+    window.memoryOnSuccess = onSuccess;
+    window.memoryOnFail = onFail;
+
+    resetMemoryState();
+    buildMemoryUI();
+    startMemoryTimer();
+}
